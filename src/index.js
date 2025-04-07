7a59f476f78e6fe6f7cacab17dae4601a595102a
ayscd <ayeskacsta@gmail.com>
2025-04-07 19:37:52 -0300

    Update 2 - display future time
    - display future time in paris timezone
    - using moment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+function displayFutureDate() {
+    let parisTime = moment().tz("Europe/Paris").add(10, "years").format(`[10 years from now, it will be] dddd, MMMM D, YYYY - HH:mm [in Paris, France]`);
+
+    let futureDate = document.querySelector("#future-date");
+    futureDate.innerHTML = parisTime;
+}
+
 function displayCurrentDate() {
     let honoluluTime = moment().tz("Pacific/Honolulu").format(`[It is] dddd, MMMM D, YYYY - HH:mm`);
     let currentDate = document.querySelector("#current-date");
@@ -5,3 +12,4 @@ function displayCurrentDate() {
 }
 
 displayCurrentDate();
+displayFutureDate();
